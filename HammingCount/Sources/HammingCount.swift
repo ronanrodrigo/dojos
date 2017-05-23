786b393ac8baedd715478accb1af18341b5df13c
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-05-22 21:53:14 -0300

    Extract normalize function to a file

--- HammingCount/Sources/HammingCount.swift
+++ HammingCount/Sources/HammingCount.swift
@@ -11,20 +11,3 @@ struct HammingCount {
     }
 
 }
-
-struct NormalizeBinaryNumbers {
-
-    static func normalize(leftBinaryNumber: String, rightBinaryNumber: String) -> (leftBinaryNumber: String, rightBinaryNumber: String) {
-        let diff = abs(rightBinaryNumber.characters.count - leftBinaryNumber.characters.count)
-        let zerosAtLeft = (0..<diff).map({ _ in "0" }).joined()
-
-        if leftBinaryNumber.characters.count < rightBinaryNumber.characters.count {
-            return (leftBinaryNumber: "\(zerosAtLeft)\(leftBinaryNumber)", rightBinaryNumber: rightBinaryNumber)
-        } else if leftBinaryNumber.characters.count > rightBinaryNumber.characters.count {
-            return (leftBinaryNumber: leftBinaryNumber, rightBinaryNumber: "\(zerosAtLeft)\(rightBinaryNumber)")
-        }
-
-        return (leftBinaryNumber: leftBinaryNumber, rightBinaryNumber: rightBinaryNumber)
-    }
-
-}
