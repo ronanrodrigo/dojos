c38ba68297ed1a514f588356b1fc31aac99bdedd
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-05-23 11:23:57 -0300

    Update String+StringAt

--- HammingCount/Sources/CodeToSubmitToLeetCode.swift
+++ HammingCount/Sources/CodeToSubmitToLeetCode.swift
@@ -1,57 +1,57 @@
 // All contet from Sources are copied here:
 
-struct HammingCount {
+// struct HammingCountX {
 
-    static func count(_ leftNumber: Int, _ rightNumber: Int) -> Int {
-        let normalizedNumbers = NormalizeBinaryNumbers.normalize(
-            leftBinaryNumber: leftNumber.toBinary(),
-            rightBinaryNumber: rightNumber.toBinary())
-        let leftBinaryNumber = normalizedNumbers.leftBinaryNumber
-        let rightBinaryNumber = normalizedNumbers.rightBinaryNumber
+//     static func count(_ leftNumber: Int, _ rightNumber: Int) -> Int {
+//         let normalizedNumbers = NormalizeBinaryNumbers.normalize(
+//             leftBinaryNumber: leftNumber.toBinary(),
+//             rightBinaryNumber: rightNumber.toBinary())
+//         let leftBinaryNumber = normalizedNumbers.leftBinaryNumber
+//         let rightBinaryNumber = normalizedNumbers.rightBinaryNumber
 
-        var counted = 0
-        for index in 0..<leftBinaryNumber.characters.count {
-            if leftBinaryNumber.string(at: index) != rightBinaryNumber.string(at: index) {
-                counted += 1
-            }
-        }
+//         var counted = 0
+//         for index in 0..<leftBinaryNumber.characters.count {
+//             if leftBinaryNumber.string(at: index) != rightBinaryNumber.string(at: index) {
+//                 counted += 1
+//             }
+//         }
 
-        return counted
-    }
+//         return counted
+//     }
 
-}
+// }
 
-struct NormalizeBinaryNumbers {
+// struct NormalizeBinaryNumbers {
 
-    static func normalize(leftBinaryNumber: String, rightBinaryNumber: String) -> (leftBinaryNumber: String, rightBinaryNumber: String) {
-        let diff = abs(rightBinaryNumber.characters.count - leftBinaryNumber.characters.count)
-        let zerosAtLeft = (0..<diff).map({ _ in "0" }).joined()
+//     static func normalize(leftBinaryNumber: String, rightBinaryNumber: String) -> (leftBinaryNumber: String, rightBinaryNumber: String) {
+//         let diff = abs(rightBinaryNumber.characters.count - leftBinaryNumber.characters.count)
+//         let zerosAtLeft = (0..<diff).map({ _ in "0" }).joined()
 
-        if leftBinaryNumber.characters.count < rightBinaryNumber.characters.count {
-            return (leftBinaryNumber: "\(zerosAtLeft)\(leftBinaryNumber)", rightBinaryNumber: rightBinaryNumber)
-        } else if leftBinaryNumber.characters.count > rightBinaryNumber.characters.count {
-            return (leftBinaryNumber: leftBinaryNumber, rightBinaryNumber: "\(zerosAtLeft)\(rightBinaryNumber)")
-        }
+//         if leftBinaryNumber.characters.count < rightBinaryNumber.characters.count {
+//             return (leftBinaryNumber: "\(zerosAtLeft)\(leftBinaryNumber)", rightBinaryNumber: rightBinaryNumber)
+//         } else if leftBinaryNumber.characters.count > rightBinaryNumber.characters.count {
+//             return (leftBinaryNumber: leftBinaryNumber, rightBinaryNumber: "\(zerosAtLeft)\(rightBinaryNumber)")
+//         }
 
-        return (leftBinaryNumber: leftBinaryNumber, rightBinaryNumber: rightBinaryNumber)
-    }
+//         return (leftBinaryNumber: leftBinaryNumber, rightBinaryNumber: rightBinaryNumber)
+//     }
 
-}
+// }
 
-extension Int {
+// extension Int {
 
-    func toBinary() -> String {
-        return String(self, radix: 2)
-    }
+//     func toBinary() -> String {
+//         return String(self, radix: 2)
+//     }
 
-}
+// }
 
-extension String {
+// extension String {
 
-    func string(at index: Int) -> String {
-        return self.characters.reduce([], { (result, char) -> [String] in
-            return result + [String(char)]
-        })[index]
-    }
+//     func string(at index: Int) -> String {
+//         return self.characters.reduce([], { (result, char) -> [String] in
+//             return result + [String(char)]
+//         })[index]
+//     }
 
-}
+// }
--- HammingCount/Sources/String+StringAt.swift
+++ HammingCount/Sources/String+StringAt.swift
@@ -1,9 +1,7 @@
 extension String {
 
     func string(at index: Int) -> String {
-        return self.characters.reduce([], { (result, char) -> [String] in
-            return result + [String(char)]
-        })[index]
+        return String(Array(self.characters)[index])
     }
 
 }
